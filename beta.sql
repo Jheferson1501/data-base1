5cbccf5809d59ef45ba32f7f888fa5a945f8a548
Jheferson <jheferson2727@gmail.com>
2025-09-12 10:21:48 -0500

    DDL table users

--- beta.sql
+++ beta.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (id SERIAL PRIMARY KEY,ide_number TEXT NOT NULL,firstname VAR
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     ide_number TEXT NOT NULL,
-    firstname VARCHAR(30) TEXT NOT NULL,
+    firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
     mobile_number TEXT NOT NULL,
     address TEXT NULL,
@@ -14,4 +14,5 @@ CREATE TABLE users (
     updated_at TIMESTAMPTZ DEFAULT now (),
     deleted_at TIMESTAMPTZ DEFAULT now ()
 );
-insert into users(ide-number,firstname,lastname,mobile_number,email,password)values('5451544','Jheferson','Rodriguez','3216248986','jheferson2727@gmail.com');
+
+ INSERT INTO users (ide_number,firstname,lastname,mobile_number, email, password  ) VALUES ('1086548635', 'Jheferson', 'Rodriguez','3216248986','jheferson2727@gmail.com','2006');
