9f4d37a77111aedfdce02b4898d64215db303680
Jheferson <jheferson2727@gmail.com>
2025-09-12 10:28:33 -0500

    DDL table users

--- beta.sql
+++ beta.sql
@@ -16,3 +16,64 @@ CREATE TABLE users (
 );
 
  INSERT INTO users (ide_number,firstname,lastname,mobile_number, email, password  ) VALUES ('1086548635', 'Jheferson', 'Rodriguez','3216248986','jheferson2727@gmail.com','2006');
+
+ Table countries {
+id integer 
+name varchar
+code varchar 
+abrev varchar 
+  created_at timestamp
+  updated_at timestamp
+  deleted_at timestamp
+}
+
+Table users {
+  id integer [primary key]
+  id_city integer 
+  id_number varchar
+  fristname varchar
+  lastname varchar 
+  addres varchar 
+  mobile_number varchar
+  email varchar 
+  password verchar 
+  status boolean
+  created_at timestamp
+  updated_at timestamp
+  deleted_at timestamp
+}
+
+Table departments {
+ id integer [primary key]
+  id_country integer 
+  id_number varchar
+  fristname varchar
+  lastname varchar 
+  addres varchar 
+  mobile_number varchar
+  email varchar 
+  password verchar 
+  status boolean
+  created_at timestamp
+  updated_at timestamp
+  deleted_at timestamp
+}
+Table cities {
+ id integer [primary key]
+  id_department integer 
+  id_number varchar
+  fristname varchar
+  lastname varchar 
+  addres varchar 
+  mobile_number varchar
+  email varchar 
+  password verchar 
+  status boolean
+  created_at timestamp
+  updated_at timestamp
+  deleted_at timestamp
+}
+
+Ref: users.id_city<cities.id
+Ref: cities.id_department<departments.id
+Ref: departments.id_country<countries.id
